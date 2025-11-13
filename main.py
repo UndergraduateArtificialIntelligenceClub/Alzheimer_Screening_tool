ee5939a14257df3877e6db062c39273f8311e3df
Jacob Garber <jacob@jwgarber.ca>
2025-11-12 21:36:00 -0700

    Make more pretty

--- main.py
+++ main.py
@@ -1,53 +1,89 @@
 import gradio as gr
-
-def show_image():
-    image_path = "cookie-theft.jpg"
-    return gr.Image(value=image_path, visible=True)
-
-def analyze_results(audio):
-    return gr.Markdown("You do not have Alzheimer's!", visible=True)
+import time
 
 info = """
-- This tool analyzes speech patterns to detect early-onset Alzheimer's.
-- An ML model was trained on [DementiaBank](https://talkbank.org/dementia/).
-- **Disclaimer**: This tool is for informational use only and does not constitute medical advice.
+## About
+Alzheimer's disease (AD) is a progressive neurodegenerative disease that impairs memory, thinking, and behaviour. It is the most common form of dementia, and initial symptoms are often confused with normal brain aging. AD is most conclusively diagnosed by positron emission tomography (PET) and cerebrospinal fluid examinations. However, these procedures are costly and invasive, and the current challenge is diagnosing Alzheimer's disease at the stage of mild cognitive impairment before memory loss occurs.
+
+## How It Works
+Symptoms of early AD often include speech impairment, object naming problems, and reduction in vocabulary. These linguistic signs can often be identified in speech patterns using NLP methods. This tool uses [BERT](https://en.wikipedia.org/wiki/BERT_(language_model))-family models trained on the ADReSS dataset from [DementiaBank](https://talkbank.org/dementia/) to estimate the likelihood of early-stage AD.
 """
 
-with gr.Blocks() as demo:
+def show_image(checkbox_state):
+    return gr.update(visible=checkbox_state)
 
-    gr.Markdown("# Alzheimer's Screening Tool")
+# This has an auto progress bar if it takes long.
+def analyze_speech(audio):
+    if audio is None:
+        return ["", ""]
+    # Simulate thinking
+    time.sleep(1)
+    return [
+            gr.Textbox("You do not have Alzheimer's!"),
+            gr.Textbox("You said blah blah blah")
+    ]
+
+def clear_all_components():
+    return [None, "", ""]
+
+with gr.Blocks(theme=gr.themes.Ocean()) as demo:
+
+    gr.Markdown("<h1 style='text-align: center;'>Alzheimer's Screening Tool</h1>")
+
+    gr.HTML("""
+    <figure>
+        <img
+          src="https://uais.dev/images/logo.svg"
+          alt="A brain made up of circuits"
+          style="display: block; margin-left: auto; margin-right: auto; max-width: 300px; max-height: 300px;">
+    </figure>
+    """)
 
     gr.Markdown(info)
 
     gr.Markdown("## Try it now!")
 
-    gr.Markdown("You will see an image. Please describe the image with as much detail as you can within 30 seconds.")
+    gr.Markdown("Start a recording, and describe the following image with as much detail as you can within 30 seconds.")
 
-    show_image_btn = gr.Button("Show Image")
+    disclaimer_checkbox = gr.Checkbox(label="I accept that this tool is for informational use only and does not constitute medical advice.")
 
-    cookie_image = gr.Image(visible=False)
-
-    audio_input = gr.Audio(
-        sources="microphone",
-        label="Record Your Speech",
-        type="filepath",
-        format="wav"
+    cookie_image = gr.Image(
+        value="cookie-theft.jpg",
+        show_label=False,
+        visible=False
     )
 
-    analyze_results_btn = gr.Button("Analyze Results")
-
-    result_text = gr.Markdown(visible=False)
-
-    show_image_btn.click(
+    disclaimer_checkbox.change(
         fn=show_image,
-        inputs=None,
-        outputs=[cookie_image]
+        inputs=disclaimer_checkbox,
+        outputs=cookie_image
     )
 
-    analyze_results_btn.click(
-        fn=analyze_results,
+    with gr.Row():
+        with gr.Column():
+            audio_input = gr.Audio(
+                sources="microphone",
+                label="Describe the image",
+            )
+
+            with gr.Row():
+                clear_btn = gr.Button("Clear")
+                analyze_btn = gr.Button("Analyze", variant="primary")
+
+        with gr.Column():
+            prediction_output = gr.Textbox(label="Prediction")
+            transcript_output = gr.Textbox(label="Transcript")
+
+    clear_btn.click(
+        fn=clear_all_components,
+        inputs=None,
+        outputs=[audio_input, prediction_output, transcript_output]
+    )
+
+    analyze_btn.click(
+        fn=analyze_speech,
         inputs=audio_input,
-        outputs=[result_text]
+        outputs=[prediction_output, transcript_output]
     )
 
 demo.queue().launch()
